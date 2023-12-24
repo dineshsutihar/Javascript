9bedf9ff19c5c65a608380d0307a6b65386fa273
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-24 15:45:06 +0000

    comparision operators

---
+++ 01_basics/06_compairsion.js
@@ -0,0 +1,48 @@
+//Comparision operators 
+// ==, ===, !=, !==, >, <, >=, <=
+
+// == (equal to)
+// === (equal value and equal type)
+// != (not equal)
+// !== (not equal value or not equal type)
+// > (greater than)
+// < (less than)
+// >= (greater than or equal to)
+// <= (less than or equal to)
+
+console.log(2 == 2); // true
+console.log(2 == "2"); // true
+console.log(2 === "2"); // false
+console.log(2 != 2); // false
+console.log(2 != "2"); // false
+console.log(2 !== "2"); // true
+
+
+console.log(2 > 2); // false
+console.log(2 < 2); // false
+console.log(2 >= 2); // true
+
+
+console.log("*******************************************************");
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(null != undefined); // false
+console.log(null !== undefined); // true
+
+console.log("*******************************************************");
+
+console.log(null == 0); // false
+console.log(null === 0); // false
+console.log(null != 0); // true
+console.log(null !== 0); // true
+
+
+console.log("*******************************************************");
+
+console.log("02" == 2); // true
+console.log("02" === 2); // false
+console.log("02" != 2); // false
+
+
+// Dont use these kind of operators which are not clear and created confusion.

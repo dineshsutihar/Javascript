34d24f4677939a016ab7583990e9b44d324f29b3
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-26 06:16:53 +0000

    Scope bacope basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+// let a = 10;
+// var b = 20;
+// const c = 30;
+
+// console.log("a: ", a);
+// console.log("b: ", b);
+// console.log("c: ", c);
+
+// if (true) {
+//   let a = 10;
+//   var b = 20;
+//   const c = 30;
+// }
+
+// console.log("a: ", a);
+// console.log("b: ", b);
+// console.log("c: ", c);
+
+// global scope => global scope is the scope outside of all the functions
+// local scope => local scope is the scope inside a function
+
+

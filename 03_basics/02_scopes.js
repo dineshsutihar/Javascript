006a7e520bb995677a9d1fa46078867b12c8ce78
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-26 06:29:10 +0000

    function detailed and scoping detailed

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -16,7 +16,44 @@
 // console.log("b: ", b);
 // console.log("c: ", c);
 
+// {} => block scope
+
 // global scope => global scope is the scope outside of all the functions
 // local scope => local scope is the scope inside a function
 
 
+// Nested scope => scope inside a scope
+
+function one(){
+    const username = "Dinesh";
+    function two(){
+        const username = "Umesh";
+        console.log("Username:inner ",username);
+    }
+    console.log("Username:outer ",username);
+    two();
+}
+one();
+
+
+if (true) {
+  const username = "Dinesh";
+  if (username === "Dinesh") {
+    const username1 = "Umesh";
+    console.log("Username:inner ",username);
+    console.log("Username1:inner ",username1);
+  }
+//   console.log("Username:outer ",username1);
+}
+
+// ++++++++++++++++++++++++++ Intersting ++++++++++++++++++++++++++++++++
+// Hoisting => hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+function addone(num){
+    return num+1;
+}
+addone(10); // it can be called before the function declaration
+
+const addTwo = function(num){ 
+    return num+2;
+}
+addTwo(20); // it cannot be called before the function declaration

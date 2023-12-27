5524dd2e085b53ab31451a23182dfd9af60a3333
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-27 16:22:21 +0000

    while and dowhile

---
+++ 05_iterations/two_while.js
@@ -0,0 +1,22 @@
+// While loop
+let index = 0;
+while (index < 10) {
+    console.log(`Index is ${index}`);
+    index += 3;
+}
+
+let myArray = ["Dinesh", "Ramesh", "Suresh", "Rajesh"];
+index = 0;
+while (index < myArray.length) {
+    console.log(`Name is ${myArray[index]}`);
+    index++;
+}
+
+
+// do while loop
+
+index = 11;
+do {
+    console.log(`Index is ${index}`);
+    index++;
+} while (index < 10);

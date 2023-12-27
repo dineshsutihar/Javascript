8205a15c65ebba1e3d3166ccc86d4a4ea5a0d715
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-27 12:56:03 +0000

    Execution context

---
+++ 03_basics/jsExecutionContext.js
@@ -0,0 +1,10 @@
+// Javascript Execution Context
+// 1. Global Execution Context *(window)
+// 2. Functional Execution Context *(function)
+// 3. Eval Execution Context *(eval)
+
+
+// Understand More deeply about working of JS: https://www.youtube.com/watch?v=ByhtOgF6uYM&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=25&t=198s
+
+
+

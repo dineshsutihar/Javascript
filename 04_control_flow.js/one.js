0b0744b62a4590d8d84a39a3e8a8675bab4ce021
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-27 14:25:42 +0000

    If else and Switch case

--- 04_control_flow.js/one.js
+++ 04_control_flow.js/one.js
@@ -18,3 +18,45 @@ if(200>100) {
 
 if(200<100) console.log('yes');
 
+
+if(200<100) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+// multiple if else
+if(200<100) {
+    console.log('yes')
+} else if(200>100) {
+    console.log('no')
+} else {
+    console.log('equal')
+}
+
+
+// && 
+if(200>100 && 200>100) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+// ||
+if(200>100 || 200<100) {
+    console.log('yes')
+} else {
+    console.log('no')
+}   
+
+// !
+if(!(200>100)) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+// Ternary operator
+// condition ? true : false
+const isUserLoggedIn1 = true
+isUserLoggedIn1 ? console.log('Welcome User') : console.log('Please login')
---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,17 @@
+//  Switch case in javascript
+
+const month = 3;
+switch (month) {
+    case 1: 
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    default:
+        console.log('Invalid month');
+        break;
+}

91412ee524334d892a5aa95b3e2a07904199c92e
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2023-12-27 13:05:07 +0000

    partial

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,20 @@
+// Is statement
+
+const isUserLoggedIn = true
+if(isUserLoggedIn) {
+    console.log('Welcome User')
+}
+
+// Condition checking operatiors :
+//  ==, ===, !=, !==, >, <, >=, <=, &&, ||, !
+
+if(1 == '1') {
+    console.log('1 == "1"')
+}
+
+if(200>100) {
+    console.log('yes')
+}
+
+if(200<100) console.log('yes');
+

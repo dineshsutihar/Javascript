a02032f3f40c434d0745cdc8044961eca8e43fe3
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-01-01 18:20:30 +0000

    Add guess validation function

--- Projects/04-GuessTheNumber/script.js
+++ Projects/04-GuessTheNumber/script.js
@@ -1,2 +1,19 @@
 const randomNumber=parseInt(Math.random() * 100 + 1); // 1-100 parseInt() is used to convert a string to an integer.
+
 const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let previousGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+function validateGuess(guess){
+    
+}

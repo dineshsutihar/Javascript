c336bbab161f5e5b7f226ef64b3ead2d3ddf3106
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-01-18 18:28:42 +0000

    keyboard done

--- 07_Projects/05-Keyborad/chaiaurcode.js
+++ 07_Projects/05-Keyborad/chaiaurcode.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert =  document.getElementById('insert');
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+    <div class="color">
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>Keycode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${event.key === " "? "Space" : e.key}</td>
+    <td>${event.keyCode}</td>
+    <td>${event.code}</td>
+  </tr>
+  
+</table>
+    </div>`
+});

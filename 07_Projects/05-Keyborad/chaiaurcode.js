63a09654b65c4df8f9e04480c1f278e76a17b4d1
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-01-18 18:32:29 +0000

    fixed typo

--- 07_Projects/05-Keyborad/chaiaurcode.js
+++ 07_Projects/05-Keyborad/chaiaurcode.js
@@ -10,7 +10,7 @@ window.addEventListener('keydown', (event) => {
     <th>Code</th>
   </tr>
   <tr>
-    <td>${event.key === " " ? "Space" : e.key}</td>
+    <td>${event.key === " " ? "Space" : event.key}</td>
     <td>${event.keyCode}</td>
     <td>${event.code}</td>
   </tr>
